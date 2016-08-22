ba0f0e176c5895cd6a9c7ee6977b3955ebff3beb
Damien <damien.daly@skazy.nc>
2016-08-22 13:39:02 +1100

    Prototype of pure C# LightFX wrapper.

---
+++ Aurora.LightFXWrapper/DllMain.cs
@@ -0,0 +1,24 @@
+﻿using Aurora.LightFXWrapper.InteropServices;
+using System;
+using System.Runtime.InteropServices;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Aurora.LightFXWrapper
+{
+    public static class DllMain
+    {
+        private static bool isInitialized = false;
+
+
+        [DllExport("LFX_GetVersion", CallingConvention.Cdecl)]
+        public static LfxResult LFX_GetVersion(StringBuilder version, uint versionSize)
+        {
+            if (!isInitialized)
+                return LfxResult.LFX_FAILURE;
+            version.Append("2.2.0.0");
+            return LfxResult.LFX_SUCCESS;
+        }
+    }
+}
---
+++ Aurora.LightFXWrapper/LfxResult.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Aurora.LightFXWrapper
+{
+    public enum LfxResult : UInt16
+    {
+        LFX_SUCCESS = 0,        // Success
+        LFX_FAILURE = 1,        // Generic failure
+        LFX_ERROR_NOINIT = 2,   // System not initialized yet
+        LFX_ERROR_NODEVS = 3,   // No devices available
+        LFX_ERROR_NOLIGHTS = 4, // No lights available
+        LFX_ERROR_BUFFSIZE = 5,	// Buffer size too small
+    }
+}
